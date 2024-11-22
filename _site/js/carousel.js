fb08f267055131be1f33ed7e69107a8b256e0418
Mario <mario.windsor@gmail.com>
2024-11-22 22:40:01 +0530

    Front-end : Functional : Carousel Buttons

--- _site/js/carousel.js
+++ _site/js/carousel.js
@@ -1,69 +1,34 @@
-
 /*
  *
- * This handles the interaction of the arrow buttons on either side of the
- * 	carousel.
+ *	Carousel
  *
  */
-$( document ).on( "click", ".js_carousel_container .js_pager", function ( event ) {
 
-	/*
-	 * 1. Get references to all the relevant elements
-	 */
-	var $carouselArrowButton = $( event.target ).closest( ".js_pager" );
-	var domCarouselContent = $carouselArrowButton
-				.closest( ".js_carousel_container" )
-				.find( ".js_carousel_content" )
-				.get( 0 );
+/* -- Scroll Controls -- */
 
-	/*
-	 * 2. Figure out the "current" carousel item, i.e. the one that's in the center
-	 */
-	// var { top, left, width, height } = domCarouselContent.getBoundingClientRect();
-	// var contentXMidpoint = left + width / 2;
-	// var contentYMidpoint = top + height / 2;
-	// var domCurrentItem = document.elementFromPoint( contentXMidpoint, contentYMidpoint );
-	// var $currentItem;
-	// if ( domCurrentItem )
-	// 	$currentItem = $( domCurrentItem ).closest( ".js_carousel_item" );
+document.addEventListener("DOMContentLoaded", function() {
+	console.log("Ready : START")
+	const scollButtons = document.querySelectorAll('.scroll-button');
+	scollButtons.forEach((scrollButton) => {
+		scrollButton.addEventListener('click', (e) => {
 
-	/*
-	 * 3. Get the "next" carousel item in the sequence
-	 * 	This could be either the preceeding or the following item,
-	 * 	depending on the arrow's direction.
-	 */
-	// var $nextItem;
-	var scrollDirection = $carouselArrowButton.data( "dir" );
-	// if ( scrollDirection == "left" )
-	// 	$nextItem = $currentItem.prev( ".js_carousel_item" );
-	// else
-	// 	$nextItem = $currentItem.next( ".js_carousel_item" );
+			var dir = scrollButton.getAttribute('data-dir');
+			var car = scrollButton.closest( ".carousel" );
+			var carList = car.querySelector( ".carousel-list" );
 
-	/*
-	 * 4. Get the amount of scroll that has to be done to center the next item
-	 */
-	// var scrollOffset;
-	// if ( $nextItem.length )
-	// 	scrollOffset = ( $nextItem.get( 0 ).offsetLeft + $nextItem.innerWidth() / 2 )
-	// 					- ( width / 2 );
-	// else	// there is no "next" item because the current one is at the boundary
-	// 	return;
+			var scrOffsetPX = window.getComputedStyle(document.documentElement).getPropertyValue('--container-width');
+			var scrOffset = parseInt(scrOffsetPX) * 0.8;
 
-	var scrollOffset = domCarouselContent.scrollLeft;
-	var $carouselItem = $( domCarouselContent ).find( ".js_carousel_item" );
-	if ( scrollDirection == "left" )
-		scrollOffset -= $carouselItem.width();
-	else
-		scrollOffset += $carouselItem.width();
+			carList.style.scrollBehavior = "smooth";
 
-	/*
-	 * 5. Finally, scroll the carousel.
-	 */
-	try {
-		domCarouselContent.scrollTo( { left: scrollOffset, behavior: "smooth" } );
-	}
-	catch ( e ) {
-		domCarouselContent.scrollTo( scrollOffset, 0 );
-	}
+			if ( dir == "left") {
+				carList.scrollLeft -= scrOffset;
+			} else if ( dir == "right") {
+				carList.scrollLeft += scrOffset;
+			}
+			
+		});
+	});
 
-} );
+});
+
