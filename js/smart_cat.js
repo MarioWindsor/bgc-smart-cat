f42a088cadc5158031472ec5ded0ae92773a5a50
MarioWindsor <mario.windsor@gmail.com>
2025-05-09 02:46:36 +0530

    update

--- js/smart_cat.js
+++ js/smart_cat.js
@@ -39,7 +39,6 @@ const fuseCat = new Fuse(allCatsData, fuseCatOptions);
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
 import { getDatabase, ref, push, onValue, get, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 const appSettings = {
-	// databaseURL: "https://bharat-glass-default-rtdb.asia-southeast1.firebasedatabase.app/"
 	databaseURL: "https://bgc-db-default-rtdb.asia-southeast1.firebasedatabase.app/"
 };
 const app = initializeApp(appSettings);
